75d73aac95f4925fbcbc1ba16eb073c57af5e6c8
Lena Meck <Lena Meck>
2016-03-17 22:05:05 +0100

    preparation for problemsolving

--- simple-todos2.js
+++ simple-todos2.js
@@ -1,29 +1,50 @@
 if (Meteor.isClient) {
 
 
-    // set questions
-    var questions = ["First question", "second question", "third question"]
-    var answers = ["Good", "I will show you the answer then", "default"]
-    Session.set("whichButton", 2)
+    // creating array: questionnaire [column][row], Questions: column = 0, Answers: column = 1 
+    var questionnaire = [
+        ["Frage 1", "Frage 2", "Frage 3"],
+        ["Antwort 1", "Antwort 2", "Antwort 3"]
+    ]
+    Session.set("Zeile", 0)
+
+
 
 
     // This code only runs on the client
     Template.body.helpers({
         showQuestions: function() {
-            return questions[0];
+            return questionnaire[0][Session.get("Zeile")];
         },
+
+        // SOLVE: only run if .no event occured
         feedback: function() {
-            whichAnswer = Session.get("whichButton")
-            return answers[whichAnswer];
+            return questionnaire[1][Session.get("Zeile")];
         }
     });
 
+
     Template.body.events({
+
+        // "I know!" --> don't show answer, just continue to next question (later: decrease frequency)
+        // SOLVE: if out of questions jump back to first question
         "click .yes": function() {
-            Session.set("whichButton", 0);
+            if (Session.get("Zeile") <= questionnaire[0].length) {
+                Session.set("Zeile", Session.get("Zeile") + 1);
+            } else {
+                Session.set("Zeile", 0)
+            }
         },
+
+        // "Show the answer, please!"
+        // SOLVE: show the answer to the question only if no-Button is clicked! (later: increase frequency)
         "click .no": function() {
-            Session.set("whichButton", 1);
+            feedback();
+        },
+
+        // SOLVE: add functionality: shows next question but no answer (later: no change in frequency)
+        "click .next": function() {
+
         }
     });
 }
